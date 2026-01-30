d638aa7ff173e347ec0e841c2cdcd1c2296a42cf
Chord-K <chordkofficial@gmail.com>
2026-01-30 10:06:50 +0400

    Add files via upload

--- constants.ts
+++ constants.ts
@@ -39,7 +39,7 @@ export const RELEASES: Release[] = [
     id: 0,
     title: "Play (Original Mix)",
     artist: "Chord K",
-    label: "Chord K",
+    label: "THE GRID Records",
     soundcloudEmbedUrl: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A2255014478&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
   },
   {
