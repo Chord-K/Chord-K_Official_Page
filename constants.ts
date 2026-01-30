b5658f2e17744b2c6e1c8429353ea933645eb63a
Chord-K <chordkofficial@gmail.com>
2026-01-30 10:00:58 +0400

    Add files via upload

--- constants.ts
+++ constants.ts
@@ -36,6 +36,13 @@ export const UPCOMING_RELEASES: UpcomingRelease[] = [
 
 export const RELEASES: Release[] = [
   {
+    id: 0,
+    title: "Play (Original Mix)",
+    artist: "Chord K",
+    label: "Chord K",
+    soundcloudEmbedUrl: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A2255014478&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+  },
+  {
     id: 1,
     title: "Hold Me Closer (Radio Edit)",
     artist: "Chord K",
